0eba7487e2f2aee2acf347cf4b128b0404ce7bf3
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-03-20 14:54:58 -0300

    Recolocados os arquivos relacionados ao trabalho 3

---
+++ Tileset.cpp
@@ -0,0 +1,8 @@
+#include "Tileset.h"
+/*
+TileSet::TileSet(int tileWidth, int tileHeight, string file): tileSet(file), tileWidth(tileWidth), tileHeight(tileHeight)
+{
+	rows= tileSet.GetHeight()/tileHeight;
+	columns= tileSet.GetWidth()/tileWidth;
+}
+*/
